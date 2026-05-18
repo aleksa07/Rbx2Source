eaf083fb1e7c4391120bf7f38aecfa3adb263aaa
aleksa07 <bogojevicaleksa07@gmail.com>
2026-05-19 00:27:58 +0200

    Bump all version refs to 2.9.2

--- Forms/Rbx2Source.Designer.cs
+++ Forms/Rbx2Source.Designer.cs
@@ -798,7 +798,7 @@
             this.MaximizeBox = false;
             this.Name = "Rbx2Source";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Rbx2Source v2.9";
+            this.Text = "Rbx2Source v2.9.2";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Rbx2Source_FormClosed);
             this.Load += new System.EventHandler(this.Rbx2Source_Load);
             this.About.ResumeLayout(false);
